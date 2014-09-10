fb8a73d10557931a4e808a818999840c4c4bd453
Netra M <netramecs@gmail.com>
2014-09-10 16:55:34 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NetApp::Application.config.secret_key_base = '65749877651cedaada49b91dd15752218d135221432dabd7ad0d2cfaf7a9f2a0e3939b040a3e099a634e8689efb49690e9c28b31d1b3d76ad2d51a99192e1e59'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NetApp::Application.config.session_store :cookie_store, key: '_net_app_session'
